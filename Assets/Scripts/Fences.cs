cebf4bcdafad02ed9e7402d862aad50408cf4677
cxworks <141250018@smail.nju.edu.cn>
2019-02-05 09:00:01 -0500

    integrate together

---
+++ Assets/Scripts/Fences.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+public class Fences : MonoBehaviour
+{
+
+    private Rigidbody rigidbody;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+
+    void OnCollisionEnter(Collision collision)
+    {
+        print(collision.gameObject.CompareTag("Player"));
+        if (collision.gameObject.CompareTag("Player")) {
+            this.rigidbody.MovePosition(rigidbody.position + new Vector3(0,40,0));
+        }
+    }
+}
---
+++ Assets/resources/fantasy-frost-environment/README.txt
@@ -0,0 +1,17 @@
+-- INSTRUCTIONS --
+
+If you are using Unity and want to get up an running right away you can open the "fantasy-frost-environment.unitypackage".
+
+If not you can import the assets one by one using their seperate folders.
+
+Each folder contains the model in .fbx format along with all of the textures.
+
+-- GUIDELINES --
+
+If you are unsure about how you are allowed to use the assets please see the Usage Guidelines: http://devassets.com/guidelines/
+
+-- HAVE FUN --
+
+I hope you will enjoy the contents of the pack!
+
+(This pack was downloaded from http://devassets.com/.)
