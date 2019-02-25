cc5c8e47d9a445cd27bff282036a48f61bbf5798
Jin Woo Lee <tpfkvla1234@gmail.com>
2019-02-25 09:14:32 -0500

    added menus

---
+++ Assets/Scripts/BackToMenu.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class BackToMenu : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void GoBackToMenu()
+    {
+        SceneManager.LoadScene("MenuScene");
+    }
+}
