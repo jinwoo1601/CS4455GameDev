bc51ede80b7365f54e1a1eba6fd07def9676918b
cxworks <141250018@smail.nju.edu.cn>
2019-03-09 21:24:26 -0500

    level 1 scene

---
+++ Assets/Scripts/Player/EnemyAI.cs
@@ -0,0 +1,76 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+[RequireComponent(typeof(UnityEngine.AI.NavMeshAgent), typeof(Animator))]
+public class EnemyAI : MonoBehaviour
+{
+    private Animator animator;
+
+    private NavMeshAgent meshAgent;
+
+    public GameObject[] waypoints;
+
+    public VelocityReporter chasing;
+
+    int currWaypoint;
+
+    public enum AIState
+    {
+        Waypoints,
+        Chasing
+    };
+
+    AIState state;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+        meshAgent = GetComponent<NavMeshAgent>();
+        currWaypoint = -1;
+        state = AIState.Waypoints;
+        setNextWaypoint();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (meshAgent.remainingDistance < 0.75f && !meshAgent.pathPending)
+        {
+            setNextWaypoint();
+        }
+        else if (state == AIState.Chasing)
+        {
+            float distance = Vector3.Distance(chasing.transform.position, this.transform.position);
+            float time = distance / meshAgent.speed;
+            Vector3 destination = chasing.transform.position + chasing.velocity * time * Time.deltaTime;
+            meshAgent.SetDestination(destination);
+        }
+        animator.SetFloat("vely", meshAgent.velocity.magnitude / meshAgent.speed);
+    }
+
+
+    private void setNextWaypoint()
+    {
+        if (waypoints.Length == 0)
+            return;
+
+
+        if (state == AIState.Waypoints)
+        {
+            currWaypoint++;
+            currWaypoint = currWaypoint % waypoints.Length;
+            meshAgent.SetDestination(waypoints[currWaypoint].transform.position);
+        }
+        else
+        {
+            currWaypoint = 0;
+            state = AIState.Waypoints;
+            meshAgent.SetDestination(waypoints[currWaypoint].transform.position);
+            return;
+        }
+
+    }
+}
---
+++ Assets/Scripts/Player/VelocityReporter.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VelocityReporter : MonoBehaviour
+{
+
+
+    private Vector3 prevPos;
+
+    public Vector3 rawVelocity
+    {
+        get;
+        private set;
+    }
+
+    public Vector3 velocity
+    {
+        get;
+        private set;
+    }
+
+    public float smoothingTimeFactor = 0.5f;
+
+    private Vector3 smoothingParamVel;
+
+    // Use this for initialization
+    void Start()
+    {
+
+        prevPos = this.transform.position;
+
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        rawVelocity = (this.transform.position - prevPos) / Time.deltaTime;
+
+        velocity = Vector3.SmoothDamp(velocity, rawVelocity, ref smoothingParamVel, smoothingTimeFactor);
+
+        prevPos = this.transform.position;
+
+    }
+}
