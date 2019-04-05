997154708e12829b70725e86242197fda5423164
unknown <mhuber@gatech.edu>
2019-04-05 13:38:59 -0400

    Added Door Prefab

---
+++ Assets/Scripts/DoorAnimator.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorAnimator : MonoBehaviour
+{
+    public Animation anim;
+
+    // void Start()
+    // {
+    //     anim = GetComponent<Animation>();
+    // }
+
+    // Update is called once per frame
+    void OnTriggerEnter(Collider c) {
+       anim.Play("Door");
+    }
+
+    void OnTriggerExit(Collider c) {
+    	anim.Play("close");
+    }
+}
