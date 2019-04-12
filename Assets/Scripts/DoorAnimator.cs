209ea1643bccb9fc7370814c91539e76535f7e12
unknown <mhuber@gatech.edu>
2019-04-11 20:57:40 -0400

    Added new dialogue and a pressable door.

--- Assets/Scripts/DoorAnimator.cs
+++ Assets/Scripts/DoorAnimator.cs
@@ -5,18 +5,27 @@ using UnityEngine;
 public class DoorAnimator : MonoBehaviour
 {
     public Animation anim;
+    public Animation animButton;
+    private int inTrigger;
 
-    // void Start()
-    // {
-    //     anim = GetComponent<Animation>();
-    // }
+    void Start()
+    {
+        inTrigger = 0;
+    }
 
     // Update is called once per frame
     void OnTriggerEnter(Collider c) {
+       inTrigger++;
        anim.Play("Door");
+       animButton.Play("Press");
     }
 
     void OnTriggerExit(Collider c) {
+    	inTrigger--;
     	anim.Play("close");
+
+    	if(inTrigger < 1) {
+    		animButton.Play("Depress");
+    	}
     }
 }
