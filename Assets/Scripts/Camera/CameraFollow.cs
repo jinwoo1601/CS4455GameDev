c34124be6d4117f3977e46427934307c0a794662
Wang, Yuhong <yuhong.wang@gatech.edu>
2019-04-18 10:48:49 -0400

    Update Camera Scripts

--- Assets/Scripts/Camera/CameraFollow.cs
+++ Assets/Scripts/Camera/CameraFollow.cs
@@ -4,20 +4,62 @@ using UnityEngine;
 
 public class CameraFollow : MonoBehaviour
 {
-    public Transform target;
-    public Vector3 camOffset;
-    public Vector3 sightOffset;
-    public float smoothRate = 0.7f;
+    public float CameraMoveSpeed = 120.0f;
+    public GameObject Follow;
+    public Vector3 FollowPos;
+    public float ClampAngle = 80.0f;
+    public float InputSensitivity = 105.0f;
+    public GameObject CameraObject;
+    public GameObject PlayerObj;
+    public float camDistanceXToPlayer;
+    public float camDistanceYToPlayer;
+    public float camDistanceZToPlayer;
+    public float mouseX;
+    public float moustY;
+    public float finalInputX;
+    public float finalInputY;
+    public float smoothX;
+    public float smoothY;
+    private float rotY = 0.0f;
+    private float rotX = 0.0f;
 
-    void LateUpdate()
+    private void Start()
     {
-        if(target != null)
-        {
-            Vector3 desiredPosition = Vector3.Lerp(transform.position, target.TransformPoint(camOffset), smoothRate);
-            transform.position = desiredPosition;
+        Vector3 rot = transform.localRotation.eulerAngles;
+        rotX = rot.x;
+        rotY = rot.y;
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+    }
 
-            Vector3 desiredLookAtPosition = target.TransformPoint(sightOffset);
-            transform.LookAt(desiredLookAtPosition);
-        }
+    private void Update()
+    {
+        float inputX = Input.GetAxis("RightStickHorizontal");
+        float inputY = Input.GetAxis("RightStickVertical");
+        mouseX = -Input.GetAxis("Mouse Y");
+        moustY = Input.GetAxis("Mouse X");
+        finalInputX = inputX + mouseX;
+        finalInputY = inputY + moustY;
+
+        rotX += finalInputX * InputSensitivity * Time.deltaTime;
+        rotY += finalInputY * InputSensitivity * Time.deltaTime;
+
+        rotX = Mathf.Clamp(rotX, -ClampAngle, ClampAngle);
+
+        Quaternion localRotation = Quaternion.Euler(rotX, rotY, 0.0f);
+        transform.rotation = localRotation;
+    }
+
+    private void LateUpdate()
+    {
+        CameraUpdate();
+    }
+
+    void CameraUpdate()
+    {
+        Transform target = Follow.transform;
+
+        float step = CameraMoveSpeed * Time.deltaTime;
+        transform.position = Vector3.MoveTowards(transform.position, target.position, step);
     }
 }
