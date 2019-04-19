99ab993161efbe614f4f6cec641fbc8298166fdd
unknown <mhuber@gatech.edu>
2019-04-19 14:17:08 -0400

    I added a porticullis that opens with space after the key is collected. It's so cute

---
+++ Assets/gateController.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class gateController : MonoBehaviour
+{
+
+	private bool animPlayed;
+	public Animation anim;
+
+
+    // Update is called once per frame
+   	void Start() {
+   		animPlayed = false;
+   	}
+
+
+    void OnTriggerStay(Collider other)
+    {
+    	KeyCollector kc = other.gameObject.GetComponent<KeyCollector>();
+        
+        if(kc != null) {
+        	if (Input.GetKeyDown("space") && other.gameObject.tag == "Player" && animPlayed == false && kc.getHasKey())
+        	{
+        		animPlayed = true;
+            	anim.Play("Raise bars");
+        	}
+        }
+    }
+}
