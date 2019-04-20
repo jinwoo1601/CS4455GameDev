8e229dd5ed1f908d7f1b7322d55c220cfed6b81c
Shuyi Sun <35696908+ssun83@users.noreply.github.com>
2019-04-20 14:31:26 -0400

    add audio stuff

---
+++ Assets/Scripts/AppEvents/AudioEventManager.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class AudioEventManager : MonoBehaviour
+{
+
+    public EventSound3D eventSound3DPrefab;
+    
+    public AudioClip[] FootstepAudio;
+
+
+    private UnityAction<Vector3> FootstepEventListener;
+
+    void Awake()
+    {
+        
+
+        FootstepEventListener = new UnityAction<Vector3>(FootstepEventHandler);
+    }
+
+
+    // Use this for initialization
+    void Start()
+    {
+
+
+        			
+    }
+
+
+    void OnEnable()
+    {
+        
+        EventManager.StartListening<FootstepEvent, Vector3>(FootstepEventListener);
+
+    }
+
+    void OnDisable()
+    {
+        EventManager.StopListening<FootstepEvent, Vector3>(FootstepEventListener);
+    }
+
+
+
+
+    void FootstepEventHandler(Vector3 pos) {
+
+
+        EventSound3D snd = Instantiate(eventSound3DPrefab, pos, Quaternion.identity, null);
+
+        snd.audioSrc.clip = this.FootstepAudio[Random.Range(0, FootstepAudio.Length)];
+
+        snd.audioSrc.minDistance = 5f;
+        snd.audioSrc.maxDistance = 100f;
+
+        snd.audioSrc.Play();
+
+
+    }
+}
