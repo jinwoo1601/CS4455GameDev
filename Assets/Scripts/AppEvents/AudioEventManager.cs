404780a898803640c507171d687e9f77383c3db9
Shuyi Sun <35696908+ssun83@users.noreply.github.com>
2019-04-20 15:50:50 -0400

    adding attack sounds + more audios

--- Assets/Scripts/AppEvents/AudioEventManager.cs
+++ Assets/Scripts/AppEvents/AudioEventManager.cs
@@ -9,15 +9,17 @@ public class AudioEventManager : MonoBehaviour
     public EventSound3D eventSound3DPrefab;
     
     public AudioClip[] FootstepAudio;
+    public AudioClip[] attackAudio;
 
 
     private UnityAction<Vector3> FootstepEventListener;
+    private UnityAction<Vector3> attackEventListener;
 
     void Awake()
     {
-        
 
         FootstepEventListener = new UnityAction<Vector3>(FootstepEventHandler);
+        attackEventListener = new UnityAction<Vector3>(attackEventHandler);
     }
 
 
@@ -32,13 +34,14 @@ public class AudioEventManager : MonoBehaviour
 
     void OnEnable()
     {
-        
+        EventManager.StartListening<attackEvent, Vector3>(attackEventListener);
         EventManager.StartListening<FootstepEvent, Vector3>(FootstepEventListener);
 
     }
 
     void OnDisable()
     {
+        EventManager.StopListening<attackEvent, Vector3>(attackEventListener);
         EventManager.StopListening<FootstepEvent, Vector3>(FootstepEventListener);
     }
 
@@ -51,6 +54,22 @@ public class AudioEventManager : MonoBehaviour
         EventSound3D snd = Instantiate(eventSound3DPrefab, pos, Quaternion.identity, null);
 
         snd.audioSrc.clip = this.FootstepAudio[Random.Range(0, FootstepAudio.Length)];
+
+        snd.audioSrc.minDistance = 5f;
+        snd.audioSrc.maxDistance = 100f;
+
+        snd.audioSrc.Play();
+
+
+    }
+
+    void attackEventHandler(Vector3 pos)
+    {
+
+
+        EventSound3D snd = Instantiate(eventSound3DPrefab, pos, Quaternion.identity, null);
+
+        snd.audioSrc.clip = this.attackAudio[Random.Range(0, attackAudio.Length)];
 
         snd.audioSrc.minDistance = 5f;
         snd.audioSrc.maxDistance = 100f;
