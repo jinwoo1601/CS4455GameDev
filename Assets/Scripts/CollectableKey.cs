5c7a2f407873272bd793314927d6b2bebfc470ba
unknown <mhuber@gatech.edu>
2019-04-20 19:07:15 -0400

    Key is now a solid object. It delivers a message to the screen and is picked up with space.

--- Assets/Scripts/CollectableKey.cs
+++ Assets/Scripts/CollectableKey.cs
@@ -4,19 +4,29 @@ using UnityEngine;
 
 public class CollectableKey : MonoBehaviour
 {
+    public DialogueManager DM;
     // code to place the key
-    void OnTriggerEnter(Collider c)
+    void OnTriggerStay(Collider c)
     {
         Debug.Log("trigger enter: " + c.tag);
         if (c.CompareTag("Player"))
         {
+            DM.DisplayMessage("Press 'Space' to pick up key");
+
             KeyCollector kc = c.gameObject.GetComponent<KeyCollector>();
             if (kc != null)
             {
-                kc.CollectKey();
-                // sound event?
-                Destroy(this.gameObject);
+                if(Input.GetKeyDown("space")){
+                    kc.CollectKey();
+                    // sound event?
+                    Destroy(this.gameObject);
+                }
             }
         }
+    }
+
+
+    void OnTriggerExit(Collider other){
+        DM.HideMessage();
     }
 }
