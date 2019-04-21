29762831a91b218a0601d811f496445e0b6a0290
Jin Woo Lee <tpfkvla1234@gmail.com>
2019-04-21 00:55:03 -0400

    2d display of coin count

--- Assets/Scripts/CoinCollector.cs
+++ Assets/Scripts/CoinCollector.cs
@@ -1,29 +1,39 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class CoinCollector : MonoBehaviour
 {
     int CoinCount;
+    public Text countText;
 
     public void Start()
     {
-
+        CoinCount = 0;
+        UpdateCountText();
     }
 
     public void CollectCoin()
     {
         CoinCount++;
+        UpdateCountText();
     }
 
     public void CollectTreasure()
     {
         int rand = Random.Range(20, 40);
         CoinCount += rand;
+        UpdateCountText();
     }
 
     public int getCoinCount()
     {
         return CoinCount;
+    }
+
+    private void UpdateCountText()
+    {
+        countText.text = CoinCount.ToString();
     }
 }
