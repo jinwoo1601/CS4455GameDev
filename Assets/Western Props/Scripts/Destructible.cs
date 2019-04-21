d55e257b84b25e44aa25d5b72c331904356b4ddf
Yuhong Wang <wangyhyh2012@gmail.com>
2019-04-21 14:12:31 -0400

    Temp

---
+++ Assets/Western Props/Scripts/Destructible.cs
@@ -0,0 +1,41 @@
+// --------------------------------------
+// This script is totally optional. It is an example of how you can use the
+// destructible versions of the objects as demonstrated in my tutorial.
+// Watch the tutorial over at http://youtube.com/brackeys/.
+// --------------------------------------
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Destructible : MonoBehaviour, Damageable {
+
+	public GameObject destroyedVersion; // Reference to the shattered version of the object
+
+    public bool canBeAttacked()
+    {
+        return true;
+    }
+
+    public Damageable getOwner()
+    {
+        return this;
+    }
+
+    public void OnDamage(Vector3 attackPoint, Vector3 attackForce)
+    {
+        destructed();
+    }
+
+    // If the player clicks on the object
+    void destructed ()
+	{
+		// Spawn a shattered object
+		Instantiate(destroyedVersion, transform.position, transform.rotation);
+		// Remove the current object
+		Destroy(gameObject);
+	}
+
+
+
+}
