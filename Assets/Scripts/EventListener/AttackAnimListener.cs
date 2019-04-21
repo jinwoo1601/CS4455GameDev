036e2fd9ba04af2b9c460d497a8f5895f3f1daf7
Yuhong Wang <wangyhyh2012@gmail.com>
2019-04-20 20:01:20 -0400

    Updated New player models

---
+++ Assets/Scripts/EventListener/AttackAnimListener.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AttackAnimListener : StateMachineBehaviour
+{
+
+    public override void OnStateEnter(Animator animator, AnimatorStateInfo animatorStateInfo, int layerIndex)
+    {
+        // Reminder: handled by animation event;
+        //PlayerInput.Instance.start_attack();
+        //BarbPlayerController.instance.m_weapon.enbaleAttack();
+    }
+
+    public override void OnStateExit(Animator animator, AnimatorStateInfo animatorStateInfo, int layerIndex)
+    {
+        // Reminder: handled by animation event;
+        //BarbPlayerController.instance.m_weapon.disableAttack();
+    }
+}
