383cb9da922f25ed69429eaf3be7b74255e03e85
unknown <mhuber@gatech.edu>
2019-04-24 01:15:25 -0400

    made cute start screen

---
+++ Assets/FlickerLight.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FlickerLight : MonoBehaviour
+{
+ public float minFlickerIntensity;
+ public float maxFlickerIntensity;
+ public float flickerSpeed;
+ private Light light;
+ 
+ private int randomizer = 1;
+ private bool increase = true;
+  
+public void Start(){
+	light = GetComponent<Light>();
+}
+
+  public void Update()
+  {
+     // if(increase == true) {
+     //   light.intensity = light.intensity + 1;
+     //   }else {
+     //   	light.intensity = light.intensity - 1;
+     //   }
+
+     //   if(light.intensity == maxFlickerIntensity) {
+     //   	increase = false;
+     //   } else if(light.intensity == minFlickerIntensity){
+     //   	increase = true;
+     //   }
+
+  	if (randomizer == 2) {
+       light.intensity = (Random.Range(minFlickerIntensity, maxFlickerIntensity));
+       randomizer = Random.Range(0, 8);
+ 
+     }
+     else {
+ 
+     randomizer = Random.Range(0, 8);
+   }
+  }
+}
