f6758b9e03039990260edb84e858b9993a03f1db
unknown <mhuber@gatech.edu>
2019-04-25 02:54:58 -0400

    buff descriptions are updated

--- Assets/Scripts/Buff.cs
+++ Assets/Scripts/Buff.cs
@@ -9,10 +9,10 @@ public class Buff
     private static int attackBuffPrice = 100;
     private static int reviveBuffPrice = 100;
 
-    private static string luckBuffDesc = "";
-    private static string speedBuffDesc = "";
-    private static string attackBuffDesc = "";
-    private static string reviveBuffDesc = "";
+    private static string luckBuffDesc = "This buff will give you extra luck. \n\nEach enemy you kill will drop a higher amount of gold!";
+    private static string speedBuffDesc = "This buff will make you faster. \n\nYou will be able to walk twice as fast!";
+    private static string attackBuffDesc = "This buff will make you stronger. \n\nYou will do more damage when you attack!";
+    private static string reviveBuffDesc = "This buff will protect you. \n\nEach time you buy this buff you will have one extra life! Use it wisely.";
 
     private static string luckBuffEffect = "buff_luck";
     private static string speedBuffEffect = "buff_speed";
