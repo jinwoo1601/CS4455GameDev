d7a099dbef77fa1785a87e857321f0af57a8dbd8
Yuhong Wang <wangyhyh2012@gmail.com>
2019-04-25 02:34:20 -0400

    Fix bugs

--- Assets/Scripts/Teleporter/Teleporter.cs
+++ Assets/Scripts/Teleporter/Teleporter.cs
@@ -6,33 +6,36 @@ public class Teleporter : MonoBehaviour
 {
 
     public string nextScene;
+    private bool startLoading = false;
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 
     private void OnTriggerEnter(Collider other)
     {
         //if (other.attachedRigidbody != null)
         //{
-            KeyCollector kc = other.gameObject.GetComponent<KeyCollector>();
-            if (kc != null)
+        KeyCollector kc = other.gameObject.GetComponent<KeyCollector>();
+        if (kc != null && !startLoading)
+        {
+            if (kc.getHasKey())
             {
-                if (kc.getHasKey())
-                {
-                    GameManager.instance.MoveToScene(nextScene);
-                } else
-                {
-                    // maybe prompt a message like "
-                }
+                GameManager.instance.MoveToScene(nextScene);
+                startLoading = true;
             }
+            else
+            {
+                // maybe prompt a message like "
+            }
+        }
         //}
     }
 }
